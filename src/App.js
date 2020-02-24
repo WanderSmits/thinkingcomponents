043729022dbaf14d698b5b2451527d0f0953f7f8
WanderSmits <wandersmits@gmail.com>
2020-02-24 17:05:50 +0100

    eindopdracht

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// App.js
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import "./App.css";
+import SectionLine from "./components/SectionLine";
+import SectionFooter from "./components/SectionFooter";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Content />
+      <SectionLine />
+      <SectionFooter />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <header id="header" className="section-header scroll">
+        <a href="./">
+          <img
+            className="icon"
+            src="https://learntocodetogether.nl/assets/icon.svg"
+          />
+        </a>
+        <a href="https://www.meetup.com/Learning-to-Code-Amsterdam/">
+          <img
+            className="meetup-icon"
+            src="https://learntocodetogether.nl/assets/meetup-icon.svg"
+          />
+        </a>
+        <nav>
+          <a href="/courses.html">Our Courses</a>
+        </nav>
+      </header>
+      <div className="section-header-spacer"></div>
+    </div>
+  );
+};
+
+export default Header;
